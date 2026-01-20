f700680ab72a7f7246de5f7df862ddbf233d1a9b
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2026-01-20 16:14:16 +0600

    feat: update layout in NewDelivery component for improved spacing

--- src/pages/dashboards/customer/NewDelivery.jsx
+++ src/pages/dashboards/customer/NewDelivery.jsx
@@ -138,8 +138,7 @@ const NewDelivery = () => {
 
   return (
     <div className="p-2 sm:p-6 md:p-8 lg:p-8">
-      {/* Header */}
-      <div className="mb-6">
+      <div className="space-y-6">
         <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl lg:text-4xl">
           Request New Delivery
         </h1>
