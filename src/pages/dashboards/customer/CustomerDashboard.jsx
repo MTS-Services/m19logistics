a84b977d9de970c669cbb32827cd3d4d1aa2fa4b
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2026-01-27 11:25:07 +0600

    refactor: remove commented header section in CustomerDashboard for cleaner code

--- src/pages/dashboards/customer/CustomerDashboard.jsx
+++ src/pages/dashboards/customer/CustomerDashboard.jsx
@@ -72,7 +72,6 @@ const CustomerDashboard = () => {
         }`}
       >
         <div className="flex h-full flex-col">
-          {/* Header */}
           <div className="flex h-28 items-center justify-center px-6">
             <Link to="/" className="transition-opacity hover:opacity-80">
               <img src="/images/logo.png" alt="M19 Logistics" className="h-22 w-auto" />
