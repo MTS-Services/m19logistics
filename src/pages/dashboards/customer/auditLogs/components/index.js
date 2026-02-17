93bcc7bce999441b937f1868d3a9ddefe2c72bed
Rakibul hasan <mdrakibulhasan12346@gmail.com>
2026-02-17 09:24:27 +0600

    refactor: remove unused AuditLogs components export file

--- src/pages/dashboards/customer/auditLogs/components/index.js
+++
@@ -1,9 +0,0 @@
-// Export all AuditLogs components for easier imports
-export { default as AuditLogsHeader } from './AuditLogsHeader';
-export { default as AuditLogsStats } from './AuditLogsStats';
-export { default as AuditLogsFilters } from './AuditLogsFilters';
-export { default as AuditLogsTable } from './AuditLogsTable';
-export { default as AuditLogsMobileCard } from './AuditLogsMobileCard';
-export { default as AuditLogsModal } from './AuditLogsModal';
-export { default as AuditLogsEmptyState } from './AuditLogsEmptyState';
-export { default as LoadingState } from './LoadingState';
